17633f0b3304de8d5df2fa070fe1235799311b36
Jonathan Meslien <34369629+meslienjonathan@users.noreply.github.com>
2018-04-20 16:49:19 +0200

    Delete poubelle.php

--- class/poubelle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        if ($this->peek()['type'] == 'PRINT') { //switch des diff function
-            $this->pop();
-            $value = $this->expect('STRING');
-            $this->expect('SEMICOLON');
-            return array('type' => 'PRINT', 'value' => $value);
-        }
-    }
